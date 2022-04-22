20127ef718a9b4e7e555475c12f057036a89d842
Ivan Pereira <navi1921@gmail.com>
2022-04-21 21:48:46 -0300

    :tada: First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Classificador gramatical
+
+1. Instale as bibliotecas do python informadas no *requirements.txt* com o comando `pip install -r requirements.txt`.
+2. Defina a varável de ambiente API_URL com a URL da API.
+3. Execute a aplicação com `streamlit run app.py`
---
+++ app.py
@@ -0,0 +1,65 @@
+import streamlit as st
+from annotated_text import annotated_text
+import requests
+import os
+
+# ===========================================#
+#                 SideBar                   #
+# ===========================================#
+# st.sidebar.title("📖 Classificador gramatical")
+# st.sidebar.markdown("Você pode escolher um das anotações abaixo:")
+
+# tagset = st.sidebar.selectbox(
+#    "Qual você prefere?", ("Spacy", "Bosque", "GSD", "Linguateca", "Macmorpho")
+# )
+
+API_URL = os.getenv("API_URL")
+
+
+def get_classification(text):
+    r = requests.get(os.path.join(API_URL, "get_classification"), params={"text": text})
+    if r.status_code == 200:
+        data = r.json()
+        tagged_words, frase_morph, tokens = (
+            data["tagged_words"],
+            data["frase_morph"],
+            data["tokens"],
+        )
+        tagged_words = [tuple(l) if type(l) == list else " " for l in tagged_words]
+        return tagged_words, frase_morph, tokens
+    else:
+        return None, None, None
+
+
+def get_tip():
+    r = requests.get(os.path.join(API_URL, "get_tip"))
+    if r.status_code == 200:
+        data = r.json()
+        return data["tip"]
+
+
+desc = "Classificador gramatical para fins didáticos!"
+
+
+def main():
+    st.info(get_tip())
+    st.title("Classificador Gramatical")
+    st.write(desc)
+    user_input = st.text_input("Informe o seu texto aqui:")
+
+    if st.button("Verificar") or user_input:
+        tagged_words, frase_morph, tokens = get_classification(user_input)
+        # print("tags: ", tagged_words)
+        if tagged_words:
+            annotated_text(*tagged_words)
+
+        # st.markdown("# Análise morfológica")
+        # if st.button("Analisar"):
+        #    for index, morph in enumerate(frase_morph):
+        #        st.markdown(f"### {tokens[index]}")
+        #        df = pd.DataFrame(morph, columns=["Categoria", "Tipo"])
+        #        st.table(df)
+
+
+if __name__ == "__main__":
+    main()
